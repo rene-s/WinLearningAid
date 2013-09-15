ccefea1e0816f649170a81e6318ab63ba840e68d
rschmidt <rene@reneschmidt.de>
2013-09-15 21:33:59 +0200

    added test files

---
+++ usr/Test-Mindmap.mm
@@ -0,0 +1,19 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1379272169891" ID="ID_1433070898" MODIFIED="1379272176591" TEXT="Test-Mindmap">
+<node CREATED="1379272178082" ID="ID_1730541525" MODIFIED="1379272265376" POSITION="right" TEXT="Was sind &quot;amtliche Werke&quot;?">
+<node CREATED="1379272183390" ID="ID_1531039417" MODIFIED="1379272284184" TEXT="&quot;Amtliche Werke sind&quot; Gesetze, Verordnungen, amtliche Erlasse und Bekanntmachungen sowie Entscheidungen und amtlich verfasste Leits&#xe4;tze zu Entscheidungen. Sie genie&#xdf;en keinen urheberrechtlichen Schutz."/>
+</node>
+<node CREATED="1379272342627" ID="ID_203512203" MODIFIED="1379272350301" POSITION="right" TEXT="&#xa7;6 Ver&#xf6;ffentliche und erschienene Werke">
+<node CREATED="1379272302499" ID="ID_1700631243" MODIFIED="1379272342167" TEXT="Ab wann gilt ein Werk als ver&#xf6;ffentlicht?">
+<node CREATED="1379272310174" ID="ID_1122922190" MODIFIED="1379272315744" TEXT="Ein Werk ist ver&#xf6;ffentlicht, wenn es mit der Zustimmung des Berechtigten der &#xd6;ffentlichkeit zug&#xe4;nglich gemacht worden ist."/>
+</node>
+<node CREATED="1379272378105" ID="ID_1494785187" MODIFIED="1379272384555" TEXT="Ab wann gilt ein Werk als erschienen?">
+<node CREATED="1379272386026" ID="ID_68693833" MODIFIED="1379272420388" TEXT="Ein Werk ist erschienen, wenn es in gen&#xfc;gender Anzahl der &#xd6;ffentlichkeit zur Verf&#xfc;gung steht."/>
+</node>
+<node CREATED="1379272422960" ID="ID_1284718243" MODIFIED="1379272435536" TEXT="Ab wann gilt ein Werk der bildenden K&#xfc;nste als erschienen?">
+<node CREATED="1379272436748" ID="ID_113048147" MODIFIED="1379272440153" TEXT="Ein Werk der bildenden K&#xfc;nste gilt als erschienen, wenn das Original oder ein Vervielf&#xe4;ltigungsst&#xfc;ck bleibend der &#xd6;ffentlichkeit zug&#xe4;nglich ist."/>
+</node>
+</node>
+</node>
+</map>
